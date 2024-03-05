2d3f14661ac610c1c96d5907ec018d35ef671560
end19 <regeneration76@yandex.ru>
2024-03-06 00:39:48 +0300

    мини фикс

--- templates/js.js
+++ templates/js.js
@@ -2,19 +2,19 @@
     {
         "name": "Энди",
         "surname": "Уир",
-        "photo": "https://sul.su/wIm",
+        "photo": "https://yandex-images.clstorage.net/e9fx7e418/cca71ela9D2/o-TFx8Z8rAZ8xzEgA4zQUDyAV5QGUj6wvp8Io19gywtYYwZ73RjWH_ivIoNR1TVss6_6-7F4DgKOCtjeozkF5_X0ltbWe_sSbgHkIgNNkNdr0JPTENMp9i4IH3JcdgiL2O3fspQFWJShMH9ID0Yge_JvPCm1vQBbXovNDbfzHXFl70fIAGZnhX0W_mbNAFydYskSWx1ULPC1zdl2iXDVkcdvVvjWjt0UL7uwhkgB5ro6YvB7dP3EUIrLTct6spT1LqPAQAojqs5yXr-myo4SHK5NnlyW1WTzvwReOsz0F1pNb1i80YkT3yQ7ssMPRmYz8Sp7t7j6zdbNQ0kBvfUVsu2tBQbPJ68B9Jq2akKJnB-jRZvfFpjoNvwE1TuDdNNSiOqQul4O01lo-bDHjUZt8PcmO3-8MYHWRYGMwf09WPJn4sRKRKljhzTX_GgOhp2S7Y_fWtBYqLH3DZkyxj8Zmk_n0f4ehJyWb7a3Co-AKzx-JLhy_TAGFgsLhIHyMtIwIeeDzQzppY18X7XhBQgSkyoGU92dUen4OgSRd474k17OKV1_GkEeVKk78IMMhKL9eWu2NnNwgZ1NCgmHt3Icvy7iAQlI5eSP8FZ7JMpAkJ-lylWb05Rg_76KHrLLOVYTzaHad9DAXxonfz3ORwbh-zVvc7b_Ms8ShICKTjMxlDrpIAMFyuKkgnDSseJGC5BXKYWY2x-Z7zc7gZg8iLSd0wCumjAdRB8boPR8hEZFI_XxJj-_O3oKFQ-LSQ20slI_Z2zLxsLlbg1yGXfsSw_SXWxOX13b1iJ_8sjW84j-U9sPp5t5kMcblG9xNA6OTSq59W-0OnswxhLMDkrOenSTvy_piIoKZGQEe1r-KI6Okp_tglZbWBujsbEL0bwMvJScQSLW9FXLUtwlOv0GR05g-r-u_jD48AQSBQdFi3o42b_uIo_OgW6rS30YNCTOBp0SpMVekF7VqzD9RJY8Qw",
         "specialties": ["Астрогеолог", "специалист по радиацонной защите"]
     },
     {
         "name": "Валера",
         "surname": "Ларионов",
-        "photo": "https://sul.su/Tne",
+        "photo": "https://avatars.yandex.net/get-music-content/2358262/82cceb59.a.12076106-1/m1000x1000?webp=false",
         "specialties": ["Программист", "прогульщик"]
     },
     {
         "name": "Григорий",
         "surname": "Мотовилович",
-        "photo": "https://sul.su/jeJ",
+        "photo": "https://narisyu.cdnbro.com/posts/8748575-inoplanetianin-risunok-poetapno-73.jpg",
         "specialties": ["Халтурщик", "бездарь"]
     }
 ]

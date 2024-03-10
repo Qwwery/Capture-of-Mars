238899670c14fc306ea13a59e02ce5838e9558a8
end19 <regeneration76@yandex.ru>
2024-03-11 02:31:55 +0300

    add register

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import os
 import random
 
 from flask import Flask, url_for, render_template, request
-from data.users import User
-from data.jobs import Jobs
+from flask import redirect
+
 from data import db_session
+from data.jobs import Jobs
+from data.users import User
+from forms.register import RegisterForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'me_secret_key:('
 
 
 def create_user(surname, name, age, position, speciality, address, email, hashed_password,
@@ -26,6 +30,7 @@ def create_user(surname, name, age, position, speciality, address, email, hashed
         user.modified_date = modified_date
     db_sess.add(user)
     db_sess.commit()
+
 
 def add_job(team_leader, job, work_size, collaborators, start_date=None, end_date=None, is_finished=None):
     db_sess = db_session.create_session()
@@ -185,6 +190,38 @@ def member():
     }
 
     return render_template('member.html', **info)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.login.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        print('ok')
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            email=form.login.data,
+            hashed_password=form.password.data,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    print(form.errors)
+    return render_template('register.html', title='Регистрация', form=form, message='Поля заполнены некорректно')
 
 
 if __name__ == '__main__':
